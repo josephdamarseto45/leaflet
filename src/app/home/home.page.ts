3d9c0d8b28b7a632e13f68e0bdeba1afe8bd7834
joseph.damarseto@gmail.com <joseph.damarseto@gmail.com>
2024-09-23 10:53:38 +0700

    popup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,11 +15,53 @@ export class HomePage {
 
   }
   ionViewDidEnter() {
+    // Base map layers
+    const openStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
 
-    this.map = L.map('mapId').setView([35.76943, -580081], 15)
+    const satellite = L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+      attribution: '&copy; <a href="https://www.esri.com/">ESRI World Imagery</a>'
+    });
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(this.map);
-  }
+    const topoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data © OpenTopoMap contributors'
+    });
+
+    const darkMap = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://carto.com/">CARTO</a>'
+    });
+
+    const streetMap = L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    });
+
+    // Inisialisasi peta dengan OpenStreetMap sebagai default
+    this.map = L.map('mapId', {
+      center: [-7.7956, 110.3695],
+      zoom: 13,
+      layers: [openStreetMap]
+    });
+
+    // Basemap options
+    const baseMaps = {
+      'OpenStreetMap': openStreetMap,
+      'Satellite': satellite,
+      'Topo Map': topoMap,
+      'Dark Map': darkMap,
+      'Street Map': streetMap
+    };
+
+    // Tambahkan kontrol layer ke peta
+    L.control.layers(baseMaps).addTo(this.map);
+
+    // Menambahkan marker di UGM Yogyakarta
+    const marker = L.marker([-7.770717, 110.377239]).addTo(this.map);
+
+    // Menambahkan pop-up yang akan muncul ketika marker diklik
+    marker.bindPopup('<b>UGM Yogyakarta</b><br>Universitas Gadjah Mada.').openPopup();
+    // Menambahkan marker di Museum Sonobudoyo Yogyakarta
+    const engineeringMarker = L.marker([-7.765394321707667, 110.37251749761643]).addTo(this.map);
+    engineeringMarker.bindPopup('<b>Fakultas Teknik</b><br>Fakultas Teknik Universitas Gadjah Mada.').openPopup();
+  }
 }
