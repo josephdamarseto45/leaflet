23f4cbce6029c0c552a6101923338726160331d7
joseph.damarseto@gmail.com <joseph.damarseto@gmail.com>
2024-09-23 10:40:01 +0700

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  map!: L.Map;
+
+  constructor() {}
+  ngOnInit() {
+
+  }
+  ionViewDidEnter() {
+
+    this.map = L.map('mapId').setView([35.76943, -580081], 15)
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.map);
+  }
+}
