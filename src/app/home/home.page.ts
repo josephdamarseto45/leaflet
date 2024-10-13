5a0a127ca1b36a305985aa0e3a1e457e136dcc86
joseph.damarseto@gmail.com <joseph.damarseto@gmail.com>
2024-10-13 08:09:54 +0700

    pop up update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,57 +11,86 @@ export class HomePage {
   map!: L.Map;
 
   constructor() {}
-  ngOnInit() {
 
-  }
+  onOnInit() {}
+
   ionViewDidEnter() {
-    // Base map layers
-    const openStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    // Mengatur tampilan lokasi peta
+    this.map = L.map('mapId').setView([-7.7744, 110.3740], 14);
+
+    // Menambahkan TileLayer
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    const customIcon = L.icon({
+      iconUrl: 'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-1024.png',
+      iconSize: [24, 24],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34]
+    });
+
+    // Marker 
+    const markerUGM = L.marker([-7.7738481733805465, 110.37232784295921], { icon: customIcon }).addTo(this.map);
+    const imageUGM = 'https://sv.ugm.ac.id/wp-content/uploads/sites/27/2021/09/Gedung-TILC-scaled.jpg'; // Sesuaikan URL gambar UGM
+
+    markerUGM.bindPopup(`
+      <b>Sekolah Vokasi Universitas Gadjah Mada</b><br>
+      Fakultas Sekolah Vokasi Universitas Gadjah Mada.<br>
+      <img src="${imageUGM}" alt="Universitas Gadjah Mada" style="max-width: 100%; height: auto;"/>
+    `).openPopup();
+
+    // Base maps
+    const satelliteLayer = L.tileLayer('https://{s}.google.com/vt?x={x}&y={y}&z={z}&s=Ga', {
+      maxZoom: 20,
+      attribution: '&copy; <a href="https://www.google.com/intl/en_us/help/terms_maps.html">Google Maps</a>',
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    });
+
+    const topoLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://opentopomap.org/copyright">OpenTopoMap</a>'
+    });
+
+    const osmLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
 
-    const satellite = L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-      attribution: '&copy; <a href="https://www.esri.com/">ESRI World Imagery</a>'
+    const cartoDBLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://carto.com/attributions">CARTO</a>'
     });
 
-    const topoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data © OpenTopoMap contributors'
+    // Base maps tambahan
+    const esriWorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     });
 
-    const darkMap = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://carto.com/">CARTO</a>'
+    const stamenWatercolor = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
+      attribution: '&copy; <a href="http://maps.stamen.com/">Stamen Maps</a>'
     });
 
-    const streetMap = L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors'
+    const esriTopoLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
     });
 
-    // Inisialisasi peta dengan OpenStreetMap sebagai default
-    this.map = L.map('mapId', {
-      center: [-7.7956, 110.3695],
-      zoom: 13,
-      layers: [openStreetMap]
+    const cartoLightLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://carto.com/attributions">CARTO</a>'
     });
 
-    // Basemap options
+    // Menambahkan layer satellite sebagai base map awal
+    satelliteLayer.addTo(this.map);
+
+    // Menambahkan layer control
     const baseMaps = {
-      'OpenStreetMap': openStreetMap,
-      'Satellite': satellite,
-      'Topo Map': topoMap,
-      'Dark Map': darkMap,
-      'Street Map': streetMap
+      'Satellite': satelliteLayer,
+      'Topographic': topoLayer,
+      'OpenStreetMap': osmLayer,
+      'CartoDB Dark Matter': cartoDBLayer,
+      'Esri World Imagery': esriWorldImagery,
+      'Stamen Watercolor': stamenWatercolor,
+      'Esri Topographic': esriTopoLayer,
+      'CartoDB Light': cartoLightLayer
     };
 
-    // Tambahkan kontrol layer ke peta
     L.control.layers(baseMaps).addTo(this.map);
-
-    // Menambahkan marker di UGM Yogyakarta
-    const marker = L.marker([-7.770717, 110.377239]).addTo(this.map);
-
-    // Menambahkan pop-up yang akan muncul ketika marker diklik
-    marker.bindPopup('<b>UGM Yogyakarta</b><br>Universitas Gadjah Mada.').openPopup();
-    // Menambahkan marker di Museum Sonobudoyo Yogyakarta
-    const engineeringMarker = L.marker([-7.765394321707667, 110.37251749761643]).addTo(this.map);
-    engineeringMarker.bindPopup('<b>Fakultas Teknik</b><br>Fakultas Teknik Universitas Gadjah Mada.').openPopup();
-  }
+  }
 }
